a9f0a31f8a6b27e20c7e964cdae9f2e482bbff16
Kit <4jrkit1@gmail.com>
2026-03-05 22:52:01 +0100

    feat : inspecting datatypes

---
+++ assignments/4kit1-glitch/chapter-2/javascript-fundamentals/solutions.js
@@ -0,0 +1,19 @@
+//  Exercise-1
+
+
+const name = "Alice";
+const age = 25;
+const isStudent = true;
+const person = { name: "Bob", age: 30 };
+const skills = ["JavaScript", "HTML", "CSS"];
+
+// inspecting types
+
+console.log(typeof name); 
+console.log(typeof age);     
+console.log(typeof isStudent);   
+console.log(typeof person);  
+console.log(Array.isArray(skills)); 
+
+
+
