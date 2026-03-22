d7cc73b5ac2372cd5f2eb84cf59b44d328ba9c4d
Josiah <josiahchefor@gmail.com>
2026-03-22 17:04:52 +0100

    feat: Completed comparison

---
+++ chapters/chapter-2-javascript-fundamentals/modules/module-1-syntax-basics/parts/CheforJosiah/practicals/1-basics/index.js
@@ -0,0 +1,81 @@
+// Let's do exponentialiszation
+
+// const a = 10;
+// const b = 2;
+
+// console.log(`EXPONENTIATION OPERATION: (a**b): ${a**b}`);
+
+// let c = 5;
+// c = c + 5;
+// console.log("The new c for increment is " + c);
+
+// c = c - 5;
+// console.log("The new c for decrement is " + c);
+
+// c++;
+// console.log("The new c using syntatic sugar for increment is " + c);
+
+// c--;
+// console.log("The new c using syntatic sugar for decrement is " + c);
+
+
+// let score = 100;
+// console.log("Initial score is " + score);
+// score += 10;
+// console.log("score after addition is " + score);
+// score = 300;
+// console.log(score);
+// score -= 10;
+// console.log(score);
+
+// COMPARISON OPERATORS IN JAVASCRIPT
+// MAKE DECISIONS BY COMPARING VALUES
+
+// console.log("COMPARISON OPERATOR");
+// const value1 = 5;
+// const value2 = "5";
+
+// const isEqual = value1 == value2;
+// console.log("is value1 equal to value2? " + isEqual);
+
+// TYPE COERCION IN JAVASCRIPT
+
+// const isStrictlyEqual = value1 === value2;
+// console.log("is value1 strictly equal to value2? " + isStrictlyEqual);
+
+// MORE OF COMPARISON OPERATORS
+const age = 13;
+const requiredAge = 18;
+const isEligible = age >= requiredAge;
+console.log("Is the person eligible? " + isEligible);
+
+// COMPARE REQUIRED AGE WITH USER INPUT NO TYPE COERCION
+// COMPARE REQUIRED AGE WITH USER INPUT WITH TYPE COERCION
+
+const userInput = "20";
+const myRequiredAge = 18;
+const isEligible1 = userInput === myRequiredAge;
+  console.log("is user eligible without type coercion? " + isEligible1);
+
+ const isEligible2 = userInput == myRequiredAge;
+  console.log("is user eligible with type coercion? " + isEligible2);
+
+
+// BASIC CALCULATOR
+console.log("BASIC CALCULATOR");
+const num1 = 10;
+const num2 = 2;
+
+const addition = num1 + num2;
+const subtraction = num1 - num2;
+const multiplication = num1 * num2;
+const division = num1 / num2;
+const modulus = num1 % num2;
+const exponential = num1 ** num2;
+
+console.log(`Addition of num1 and num2 is: ${num1} + ${num2} = ${addition}`);
+console.log(`Subtraction of num1 and num2 is: ${num1} - ${num2} = ${subtraction}`);
+console.log(`Multiplication of num1 and num2 is: ${num1} * ${num2} = ${multiplication}`);
+console.log(`Division of num1 and num2 is: ${num1} / ${num2} = ${division}`);
+console.log(`Modulus is ${num1} % ${num2} = ${modulus}`);
+console.log(`Exponential is ${num1} ** ${num2} = ${exponential}`);
