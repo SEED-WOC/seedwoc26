76546bb16ede15aff32f0a6b381f9edafcf6f895
THE EYE INFORMATIQUE <iwstechnical7@gmail.com>
2026-03-22 15:07:10 +0100

    done with basics of data types in js

---
+++ chapters/chapter-2-javascript-fundamentals/modules/module-1-syntax-basics/parts/gita/practicals/1_basics/index.js
@@ -0,0 +1,23 @@
+const name="Gita";
+// name="Paul";
+
+
+const movieTitle="The Matrix";
+const anotherMovieTitle='Inception';
+// for number
+// we can int or float-> numbers
+const favNumber=5;
+const pi=3.14;
+
+// console.log("TYPE OF FAVNUMBER:", typeof favNumber);
+// console.log("Type of PI",typeof pi);
+// undefined vs null
+let username=null;
+console.log("username:", username);
+
+if (username){
+    console.log("Username is defined");
+}
+else {
+    console.log("Username is undefined");
+}
